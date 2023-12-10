75467b41ab98e6ca4320a445e9bf21a52083490f
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2023-12-10 04:55:26 -0800

    Removing rules option consts because unused and undesirable

--- src/package/language-rulesets/rulesets/ruleset/values/index.ts
+++ src/package/language-rulesets/rulesets/ruleset/values/index.ts
@@ -1,14 +1,3 @@
 type Severity = "error" | "warn" | "off";
-enum Option {
-  always,
-  never,
-  any,
-  explicit,
-  consistent,
-  "as-needed",
-  "no-public",
-  "*",
-}
 
 export type { Severity };
-export { Option };
