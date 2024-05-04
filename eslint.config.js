1a3e752cef6547338b188632539eebfed1e2ccd6
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-05-03 23:25:57 -0700

    npm: remove test svelte dependencies; project lint: rm test svelte ref

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,7 @@
 // PLUGINS
 import stylistic from "@stylistic/eslint-plugin";
 import ts from "@typescript-eslint/eslint-plugin";
-import svelte from "eslint-plugin-svelte";
 import tsParser from "@typescript-eslint/parser";
-import svelteParser from "svelte-eslint-parser";
 import rulesJTsStylistic from "./stylistic.base.config.js";
 import rulesJs from "./eslint.js.config.js";
 import rulesTsDisableCompiler from "./eslint.ts.disable.compiler.config.js";
@@ -23,7 +21,6 @@ const OPTIONS = {
       "eslint.config.js",
     ],
     ts: ["src/**/*.ts"],
-    svelte: ["src/**/*.svelte"],
   },
   plugins: {
     js: { "@stylistic": stylistic },
@@ -31,12 +28,6 @@ const OPTIONS = {
       return {
         ...OPTIONS.plugins.js,
         "@typescript-eslint": ts,
-      };
-    },
-    get svelte() {
-      return {
-        ...OPTIONS.plugins.ts,
-        svelte,
       };
     },
   },
@@ -47,9 +38,6 @@ const OPTIONS = {
     },
     get ts() {
       return OPTIONS.linterOptions.js;
-    },
-    get svelte() {
-      return OPTIONS.linterOptions.ts;
     },
   },
   languageOptions: {
@@ -67,22 +55,10 @@ const OPTIONS = {
         },
       };
     },
-    get svelte() {
-      return {
-        ...OPTIONS.languageOptions.js,
-        parser: svelteParser,
-        parserOptions: {
-          ...OPTIONS.languageOptions.ts.parserOptions,
-          extraFileExtensions: [".svelte"],
-          parser: OPTIONS.languageOptions.ts.parser,
-        },
-      };
-    },
   },
   processor: {
     js: {},
     ts: {},
-    svelte: { processor: "svelte/svelte" },
   },
 };
 const RULESET = {
@@ -97,13 +73,6 @@ const RULESET = {
       rulesTsDisableExtend,
       rulesTsEnableExtend,
       rulesTsEnable,
-    ];
-  },
-  get svelte() {
-    return [
-      ...RULESET.ts,
-      svelte.configs["flat/all"][1].rules,
-      svelte.configs["flat/all"][2].rules,
     ];
   },
 };
@@ -129,5 +98,4 @@ function flattenRuleset(
 export default [
   ...flattenRuleset("js"),
   ...flattenRuleset("ts"),
-  ...flattenRuleset("svelte"),
 ];
