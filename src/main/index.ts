756da2a4db53c4c5f7efbb95e550efaf0b1c0c65
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-05-05 23:50:37 -0700

    flippety floopety

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,9 +1,9 @@
-import JsOptions from "./options/JsOptions.js";
-import TsOptions from "./options/TsOptions.js";
-import SvelteOptions from "./options/SvelteOptions.js";
 import JsRuleset from "./rulesets/JsRuleset.js";
 import TsRuleset from "./rulesets/TsRuleset.js";
 import SvelteRuleset from "./rulesets/SvelteRuleset.js";
+import JsOptions from "./options/JsOptions.js";
+import TsOptions from "./options/TsOptions.js";
+import SvelteOptions from "./options/SvelteOptions.js";
 
 declare type _RequiredLanguage =
   | "js"
