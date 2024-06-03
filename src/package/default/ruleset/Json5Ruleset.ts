9db4b3ca82857c38d7cdec1d7909466516c20d22
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-06-03 12:30:22 -0700

    Chore: add doc links to configure Json rules

--- src/package/default/ruleset/Json5Ruleset.ts
+++ src/package/default/ruleset/Json5Ruleset.ts
@@ -1,3 +1,6 @@
 const Json5Ruleset: IRules[] = [];
 
+// https://github.com/ota-meshi/eslint-plugin-jsonc/blob/master/lib/configs/flat/base.ts
+// https://ota-meshi.github.io/eslint-plugin-jsonc/rules/
+
 export default Json5Ruleset;
--- src/package/default/ruleset/JsoncRuleset.ts
+++ src/package/default/ruleset/JsoncRuleset.ts
@@ -1,3 +1,6 @@
 const JsoncRuleset: IRules[] = [];
 
+// https://github.com/ota-meshi/eslint-plugin-jsonc/blob/master/lib/configs/flat/base.ts
+// https://ota-meshi.github.io/eslint-plugin-jsonc/rules/
+
 export default JsoncRuleset;
