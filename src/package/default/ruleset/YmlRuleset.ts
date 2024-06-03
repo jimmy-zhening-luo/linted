50c07580e9c09819fd4a4b3550132703c54f892f
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-06-03 13:27:54 -0700

    typo

--- src/package/default/ruleset/YmlRuleset.ts
+++ src/package/default/ruleset/YmlRuleset.ts
@@ -2,7 +2,7 @@ const YmlRuleset: IRules[] = [];
 
 // https://redhat.com/en/topics/automation/what-is-yaml
 // https://yaml.org/
-// https:// github.com/ota-meshi/eslint-plugin-yml/blob/master/src/configs/flat/base.ts
+// https://github.com/ota-meshi/eslint-plugin-yml/blob/master/src/configs/flat/base.ts
 // https://ota-meshi.github.io/eslint-plugin-yml/rules/
 
 export default YmlRuleset;
