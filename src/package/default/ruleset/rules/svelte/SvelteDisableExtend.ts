6a9c27f3f7a2effcfcb5825610d4b63e96287bf7
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-06-03 14:10:53 -0700

    disable extension: remove unecessary ESLint deprecation tag for stylistic

--- src/package/default/ruleset/rules/svelte/SvelteDisableExtend.ts
+++ src/package/default/ruleset/rules/svelte/SvelteDisableExtend.ts
@@ -5,7 +5,6 @@ const SvelteDisableExtend: IRules = {
   // http://sveltejs.github.io/eslint-plugin-svelte/rules/#extension-rules
   "no-inner-declarations": OFF, // also disabled in SvelteDisableConflictJs per Svelte base config
 
-  // "no-trailing-spaces": OFF, // ESLint DEPRECATED -> @stylistic
   // "@stylistic/no-trailing-spaces": OFF,
 
 };
