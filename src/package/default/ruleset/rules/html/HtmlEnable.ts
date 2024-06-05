bae5d47714e4aa409423b67ce3d1d27522c77591
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-06-04 19:48:53 -0700

    don't require title

--- src/package/default/ruleset/rules/html/HtmlEnable.ts
+++ src/package/default/ruleset/rules/html/HtmlEnable.ts
@@ -1,3 +1,10 @@
-const HtmlEnable: IRules = {};
+const [
+  ,
+  OFF,
+] = [
+  "error",
+  "off",
+] as const;
+const HtmlEnable: IRules = { "@html-eslint/require-title": OFF };
 
 export default HtmlEnable;
