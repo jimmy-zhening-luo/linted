ecdce7466b0e28df6613af2e6506231c9d42804e
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-06-04 17:06:11 -0700

    -JsonDisableConflictExtend because no base rules nor stylistic (formatting) rules (with removal of plugin) will be used

--- src/package/default/ruleset/Json5Ruleset.ts
+++ src/package/default/ruleset/Json5Ruleset.ts
@@ -1,11 +1,7 @@
-import Json5DisableConflict from "./rules/json5/Json5DisableConflict.js";
-import Json5DisableExtend from "./rules/json5/Json5DisableExtend.js";
 import Json5EnableExtend from "./rules/json5/Json5EnableExtend.js";
 import Json5Enable from "./rules/json5/Json5Enable.js";
 
 const Json5Ruleset: IRules[] = [
-  Json5DisableConflict,
-  Json5DisableExtend,
   Json5EnableExtend,
   Json5Enable,
 ];
