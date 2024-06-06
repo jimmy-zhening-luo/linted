c883e5b75ef153b7505194b40157b55bf7e8fd83
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-06-06 00:14:27 -0400

    chore/no behavior change: rules/yml: -disable, no intention to enable base rules

--- src/package/default/ruleset/YmlRuleset.ts
+++ src/package/default/ruleset/YmlRuleset.ts
@@ -1,11 +1,7 @@
-import YmlDisableConflict from "./rules/yml/YmlDisableConflict.js";
-import YmlDisableExtend from "./rules/yml/YmlDisableExtend.js";
 import YmlEnableExtend from "./rules/yml/YmlEnableExtend.js";
 import YmlEnable from "./rules/yml/YmlEnable.js";
 
 const YmlRuleset: IRules[] = [
-  YmlDisableConflict,
-  YmlDisableExtend,
   YmlEnableExtend,
   YmlEnable,
 ];
