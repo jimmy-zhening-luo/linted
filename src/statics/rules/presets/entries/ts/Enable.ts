a89b8f700f16cdc02608d6f7ed1bbe52988c0eaf
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-07-31 14:06:51 -0700

    v9: ts/no-floating-promises: +opt: check-thenables

--- src/statics/rules/presets/entries/ts/Enable.ts
+++ src/statics/rules/presets/entries/ts/Enable.ts
@@ -322,6 +322,7 @@ export default [
     "@typescript-eslint/no-floating-promises": [
       ERROR,
       {
+        checkThenables: true,
         ignoreVoid: true,
         ignoreIIFE: false,
       },
