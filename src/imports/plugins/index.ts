853f287ae17ded978bf0f1a4cb608776131a8846
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-07-31 12:07:36 -0700

    align name to plugin name actually

--- src/imports/plugins/index.ts
+++ src/imports/plugins/index.ts
@@ -1,19 +1,19 @@
 import type { Boundary } from "@eslinted/core";
 import stylistic from "@stylistic/eslint-plugin";
-import ts from "@typescript-eslint/eslint-plugin";
+import typescript_eslint from "@typescript-eslint/eslint-plugin";
 import svelte from "eslint-plugin-svelte";
 import mocha from "eslint-plugin-mocha";
-import html from "@html-eslint/eslint-plugin";
+import html_eslint from "@html-eslint/eslint-plugin";
 import jsonc from "eslint-plugin-jsonc";
 import yml from "eslint-plugin-yml";
 import markdownlint from "eslint-plugin-markdownlint";
 
 export default {
   "@stylistic": stylistic,
-  "@typescript-eslint": ts as unknown as { configs: unknown },
+  "@typescript-eslint": typescript_eslint as unknown as { configs: unknown },
   svelte,
   mocha,
-  "@html-eslint": html,
+  "@html-eslint": html_eslint,
   jsonc,
   yml,
   markdownlint,
