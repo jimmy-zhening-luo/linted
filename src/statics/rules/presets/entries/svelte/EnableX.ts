ffc209c4218f738d64fe575fe17d2dc6b7f3dfc1
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-07-31 13:36:20 -0700

    v9: svelte: no-inner-declarations align to js (+option:blockscopedfunc)

--- src/statics/rules/presets/entries/svelte/EnableX.ts
+++ src/statics/rules/presets/entries/svelte/EnableX.ts
@@ -1,6 +1,7 @@
 import type RuleEntry from "../entry.js";
 import { ERROR, OFF } from "../level.js";
 import {
+  ALLOW,
   BOTH,
 } from "../state.js";
 import { EnableX } from "../id.js";
@@ -13,6 +14,7 @@ export default [
     "svelte/no-inner-declarations": [
       ERROR,
       BOTH,
+      { blockScopedFunctions: ALLOW },
     ], /* Same options as in JsEnable */
     "svelte/no-trailing-spaces": OFF, /* Only applies to HTML comments, which I don't intend to use: http://sveltejs.github.io/eslint-plugin-svelte/rules/no-trailing-spaces/ */
   },
