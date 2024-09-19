3a5e4a7c6a81cc6eb6823a54999ce799045f9f21
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-09-18 20:33:13 -0700

    rule/html: element-newline: skip head due to bad breaking change from html plugin

--- src/statics/rules/presets/html/Enable.ts
+++ src/statics/rules/presets/html/Enable.ts
@@ -1,5 +1,11 @@
 import { Strings, type RuleEntry } from "../index.js";
 
-const { Id: { Enable }, Level: { OFF } } = Strings;
+const { Id: { Enable }, Level: { ERROR, OFF } } = Strings;
 
-export default [Enable, { "@html-eslint/require-title": OFF }] as const satisfies RuleEntry;
+export default [
+  Enable,
+  {
+    "@html-eslint/require-title": OFF,
+    "@html-eslint/element-newline": [ERROR, { skip: ["head"] }],
+  },
+] as const satisfies RuleEntry;
