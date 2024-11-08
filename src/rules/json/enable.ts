750a92bd6e7dcbdc55a51db98c3d0b6854e71325
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-11-07 19:09:12 -0800

    json:   "json/no-empty-keys" is a stupid and useless rule considering their own disclaimer that package-lock.json uses empty keys (and also the fact that they're 100% chill and allowed)

--- src/rules/json/enable.ts
+++ src/rules/json/enable.ts
@@ -1,12 +1,12 @@
 import { enable, level } from "../../_strings";
 
-const { ERROR } = level;
+const { ERROR, OFF } = level;
 
 export default [
   enable,
   {
     // https://github.com/eslint/json?tab=readme-ov-file#rules
     "json/no-duplicate-keys": ERROR,
-    "json/no-empty-keys": ERROR,
+    "json/no-empty-keys": OFF,
   },
 ] as const;
