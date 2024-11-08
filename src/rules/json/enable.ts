5ac13da9aca23d5e7a5bb0f225e0c7db729e293e
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-11-07 19:05:43 -0800

    scrub shit, no-unsafe-values doesn't even exist yet eslint just doesn't give a fuck about main branch sanitization

--- src/rules/json/enable.ts
+++ src/rules/json/enable.ts
@@ -8,6 +8,5 @@ export default [
     // https://github.com/eslint/json?tab=readme-ov-file#rules
     "json/no-duplicate-keys": ERROR,
     "json/no-empty-keys": ERROR,
-    "json/no-unsafe-values": ERROR,
   },
 ] as const;
