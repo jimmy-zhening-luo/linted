c47e4c2cbd006fcd55e5c3f609f8dc3916c7738a
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-11-12 11:47:36 -0800

    */ignores: +extend default .git -> ignore in all directories, +built folders, +Trash

--- src/ignores/global.ts
+++ src/ignores/global.ts
@@ -1,1 +1,12 @@
-export default ["**/*package-lock.json"];
+export default [
+  // "**/node_modules/", /* ESLint @default */,
+  // ".git/", /* ESLint @default */,
+  "{,**/.git/}",
+  "**/*package-lock.json",
+  "**/dist/",
+  "**/buil{d,t}/",
+  "**/bundle{,d}/",
+  "**/pack{,ed}/",
+  "{,**/.svelte-kit/}",
+  "{,**/.Trash/}",
+];
