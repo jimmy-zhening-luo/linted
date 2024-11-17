11c920a008660a8c04803303b5d8dde5ccc0eaf4
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-11-17 12:07:08 -0800

    selfhost core 13.4.2; add basic testing; rev defaults 3.1.0
    - turned off style rule for lines around comments, just plain annoying

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,11 +1,25 @@
 import { expect } from "chai";
-import core from "./";
+import linted from ".";
+import { ignores } from "@eslinted/defaults";
 
-describe("Core", function () {
+const configs = linted(),
+numScopes = Object.keys(ignores).length - 1;
+
+describe("Linted", function () {
   describe("shape", function () {
     it("is function", function () {
-      expect(core)
+      expect(linted)
         .a("function");
+    });
+  });
+  describe("output", function () {
+    it("is array", function () {
+      expect(configs)
+        .an("array");
+    });
+    it(`has length >= settings + plugins + * + ${numScopes} scopes) = ${3 + numScopes}  [Actual: ${configs.length}]`, function () {
+      expect(configs)
+        .has.lengthOf.at.least(3 + numScopes);
     });
   });
 });
