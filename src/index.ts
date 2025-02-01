c3c16d8cdb93ced273a81ee5156e097a6525c307
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2025-02-01 01:50:21 -0800

    recomment; bump pkg (cd)

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import Core, { type Input } from "@eslinted/core";
 import * as defaults from "@eslinted/defaults";
 import { plugins } from "./plugins";
 import { parsers } from "./parsers";
-// import type { Config } from "typescript-eslint";
+// import type { Linter } from "eslint";
 
 export default function (extensions: Input["extensions"] = {}): readonly unknown[] {
   try {
