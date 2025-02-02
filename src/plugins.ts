2ddfd860db2a0b24a1a4a19a8c8dc89dd9c837c9
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2025-02-02 12:13:27 -0800

    plumb css

--- src/plugins.ts
+++ src/plugins.ts
@@ -1,21 +1,23 @@
 import stylistic from "@stylistic/eslint-plugin";
 import { plugin as typescript_eslint } from "typescript-eslint";
-import svelte from "eslint-plugin-svelte";
 import mocha from "eslint-plugin-mocha";
 import chai_friendly from "eslint-plugin-chai-friendly";
 import chai_expect from "eslint-plugin-chai-expect";
+import svelte from "eslint-plugin-svelte";
 import html_eslint from "@html-eslint/eslint-plugin";
+import css from "@eslint/css";
 import jsonc from "eslint-plugin-jsonc";
 import yml from "eslint-plugin-yml";
 
 export const plugins: Readonly<Record<string, Partial<Record<"configs", unknown>>>> = {
   "@stylistic": stylistic,
   "@typescript-eslint": typescript_eslint,
-  svelte,
   mocha,
   "chai-friendly": chai_friendly,
   "chai-expect": chai_expect,
+  svelte,
   "@html-eslint": html_eslint,
+  css,
   jsonc,
   yml,
 };
