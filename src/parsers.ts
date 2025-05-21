632244940002d3891851d3b6a729f0eb025af708
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-05-21 15:11:10 -0700

    json language support

--- src/parsers.ts
+++ src/parsers.ts
@@ -1,15 +1,11 @@
 import { parser as ts } from "typescript-eslint";
 import svelte from "svelte-eslint-parser";
 import html from "@html-eslint/parser";
-import jsonc from "jsonc-eslint-parser";
 import yml from "yaml-eslint-parser";
 
-type Parsers = "ts" | "svelte" | "html" | "jsonc" | "yml";
-
-export const parsers: Readonly<Record<Parsers, unknown>> = {
+export const parsers = {
   ts,
   svelte,
   html,
-  jsonc,
   yml,
 };
