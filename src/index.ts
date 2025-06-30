48c67a179d51a35bd37dc6b7e71556def73addc2
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-06-30 11:42:08 -0700

    prepare core 19 schema pt 1

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,17 @@ import Core from "@eslinted/core";
 import * as defaults from "@eslinted/defaults";
 import * as imports from "./imports";
 
-export default function (extensions: Parameters<typeof Core>[0]["extensions"] = {}) {
+export default function (
+  extensions: Parameters<typeof Core>[0]["configuration"]["extensions"] = {},
+) {
   try {
     return Core(
       {
         imports,
-        defaults,
-        extensions,
+        configuration: {
+          defaults,
+          extensions,
+        },
       },
     );
   }
