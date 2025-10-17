a3da981beb1f62c6b2adf65bfda0a5a719f4e8d3
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-10-16 17:16:15 -0700

    -remove try wrapper

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,13 @@ import imports from "./imports";
 export default function (
   extensions: Parameters<typeof Core>[0]["configuration"]["extensions"] = {},
 ) {
-  try {
-    return Core(
-      {
-        imports,
-        configuration: {
-          defaults,
-          extensions,
-        },
+  return Core(
+    {
+      imports,
+      configuration: {
+        defaults,
+        extensions,
       },
-    );
-  }
-  catch (e) {
-    throw TypeError(
-      "linted: ",
-      { cause: e },
-    );
-  }
+    },
+  );
 }
