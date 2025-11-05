d8419d945d744f9b7d26ac1536ce8f90eaeca3fb
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-11-04 23:45:22 -0800

    Update eslint-plugin-jsonc module declarations

--- typings/jsonc.d.ts
+++ typings/jsonc.d.ts
@@ -1,3 +1,7 @@
-declare module "eslint-plugin-jsonc" {
-  export const configs: unknown;
+declare module "eslint-plugin-jsonc/meta" {
+  export const name: "eslint-plugin-jsonc";
+}
+
+declare module "eslint-plugin-jsonc/types" {
+  //
 }
